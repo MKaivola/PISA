70b0189f87b8662d226e98b509f3705c5b33e48d
Mikko Kaivola <mkaivola96@gmail.com>
2024-10-16 15:58:25 +0300

    Extract codes and select PISA quantity to model

--- main.R
+++ main.R
@@ -4,30 +4,46 @@ library(tidyr)
 year_code <- "X2018..YR2018."
 
 # Initial columns to extract
-cols_to_extract <- c("Country.Name", "Series.Name", year_code)
+cols_to_extract <- c("Country.Name", "Series.Code",  year_code)
 
 read_and_widen_csv <- function(filename) {
   
-  develop_df <- read.csv(filename)
+  df <- read.csv(filename)
   
-  develop_df <- develop_df[cols_to_extract]
+  df[,year_code] <- as.numeric(df[,year_code])
   
-  develop_df[,year_code] <- as.numeric(develop_df[,year_code])
+  df <- df[!is.na(df[,year_code]),]
   
-  develop_df <- develop_df[!is.na(develop_df[,year_code]),]
+  df_codes <- unique(df[c("Series.Name", "Series.Code")])
   
-  develop_df <- tidyr::pivot_wider(develop_df, id_cols = Country.Name, 
-                                   names_from = Series.Name, 
-                                   values_from = year_code)
+  df <- df[cols_to_extract]
   
-  return(develop_df)
+  df <- tidyr::pivot_wider(df, id_cols = Country.Name, 
+                                   names_from = Series.Code, 
+                                   values_from = all_of(year_code))
+  
+  return(list(data = df, codes = df_codes))
   
 }
 
-csv_filenames <- list.files("data", full.names = T)
 
-df_list <- lapply(csv_filenames, read_and_widen_csv)
 
-df <- Reduce(function(x,y) merge(x,y, by="Country.Name"), df_list)
+PISA_filename <- list.files("data/PISA_results", full.names = T)
+
+PISA_data <- read_and_widen_csv(PISA_filename)
+
+PISA_math_below_1 <- PISA_data$data[c("Country.Name", "LO.PISA.MAT.0")]
+
+covariate_filenames <- list.files("data/Covariate_csv", full.names = T)
+
+covariate_data <- lapply(covariate_filenames, read_and_widen_csv)
+
+covariate_dfs <- lapply(covariate_data, function(x) x$data)
+
+covariate_codes <- do.call(rbind,lapply(covariate_data, function(x) x$codes))
+
+df <- Reduce(function(x,y) merge(x,y, by="Country.Name", all.x = T), c(list(PISA_math_below_1), covariate_dfs))
+
+
 
 
