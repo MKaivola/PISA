a2cb89ef1a8d20edea9308231e6dcfa8f1c99879
Mikko Kaivola <mkaivola96@gmail.com>
2024-10-16 16:30:29 +0300

    Simple filtering of missing data

--- main.R
+++ main.R
@@ -21,7 +21,6 @@ read_and_widen_csv <- function(filename) {
   df <- tidyr::pivot_wider(df, id_cols = Country.Name, 
                                    names_from = Series.Code, 
                                    values_from = all_of(year_code))
-  
   return(list(data = df, codes = df_codes))
   
 }
@@ -44,6 +43,32 @@ covariate_codes <- do.call(rbind,lapply(covariate_data, function(x) x$codes))
 
 df <- Reduce(function(x,y) merge(x,y, by="Country.Name", all.x = T), c(list(PISA_math_below_1), covariate_dfs))
 
+rownames(df) <- df$Country.Name
+
+df <- df[!(names(df) %in% c("Country.Name"))]
 
 
+# Calculate the proportion of missing values for each variable
+
+props_NA_var <- apply(df, 2, function(col) mean(is.na(col)))
+
+sort(props_NA_var)
+
+# Calculate the proportion for each country
+
+props_NA_obs <- apply(df, 1, function(col) mean(is.na(col)))
+
+sort(props_NA_obs)
+
+# Drop variables for which proportion exceeds 10 %
+
+vars_to_drop <- names(props_NA_var[props_NA_var > 0.1])
+
+df_subset <- df[!(names(df) %in% vars_to_drop)]
+
+# Drop obs where there are missing values
+
+obs_to_drop <- !apply(df_subset, 1, function(row) any(is.na(row)))
+
+df_subset <- df_subset[obs_to_drop, ]
 
