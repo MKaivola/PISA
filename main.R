e62f24db92fc2870051e92d9b4c87941081468dd
Mikko Kaivola <mkaivola96@gmail.com>
2024-10-18 11:55:45 +0300

    Summary stats and bootstrap CIs

--- main.R
+++ main.R
@@ -1,10 +1,16 @@
 library(tidyr)
+library(moments)
+library(boot)
 
 # Code for Year 2018
 year_code <- "X2018..YR2018."
 
 # Initial columns to extract
 cols_to_extract <- c("Country.Name", "Series.Code",  year_code)
+
+# Relevant PISA series code
+
+PISA_code <- "LO.PISA.MAT.0"
 
 read_and_widen_csv <- function(filename) {
   
@@ -31,7 +37,7 @@ PISA_filename <- list.files("data/PISA_results", full.names = T)
 
 PISA_data <- read_and_widen_csv(PISA_filename)
 
-PISA_math_below_1 <- PISA_data$data[c("Country.Name", "LO.PISA.MAT.0")]
+PISA_math_below_1 <- PISA_data$data[c("Country.Name", PISA_code)]
 
 covariate_filenames <- list.files("data/Covariate_csv", full.names = T)
 
@@ -71,4 +77,32 @@ df_subset <- df[!(names(df) %in% vars_to_drop)]
 obs_to_drop <- !apply(df_subset, 1, function(row) any(is.na(row)))
 
 df_subset <- df_subset[obs_to_drop, ]
+
+### Univariate analysis ###
+
+basic_sum_stats <- c(mean, median, sd, function(x) mad(x, constant = 1.0), moments::skewness)
+
+names(basic_sum_stats) <- c("Mean", "Median", "SD", "MAD", "Skew")
+
+df_sum_stats <- sapply(basic_sum_stats, function(func) apply(df_subset, 2, func))
+
+var_inds <- 1:ncol(df_subset)
+
+names(var_inds) <- colnames(df_subset)
+
+boot_ci_func <- function(data, stat) {
+  boot_obj <- boot::boot(data, function(d, ind) apply(data, 2, function(col) stat(col[ind])), 10000, parallel = "snow")
+  
+  boot_cis <- sapply(var_inds, function(ind) boot::boot.ci(boot_obj, index = ind, type = "basic")$basic[4:5])
+  
+  return(boot_cis)
+}
+
+boot_cis <- lapply(basic_sum_stats, function(stat) boot_ci_func(df_subset, stat))
+
+PISA_sum_cis <- sapply(boot_cis, function(mat) mat[,PISA_code])
+
+PISA_sum_stats <- rbind(df_sum_stats[PISA_code,], PISA_sum_cis)
+
+rownames(PISA_sum_stats) <- c("Est.","Lower", "Upper")
 
