fcb0d9eef3a02c9b4f7948d460491e0522201726
Mikko Kaivola <mkaivola96@gmail.com>
2024-11-20 17:07:53 +0200

    Boosted tree tuning and assessing feature outcome relationships

--- tidy_feat_eng_and_model.R
+++ tidy_feat_eng_and_model.R
@@ -1,6 +1,8 @@
 library(readr)
 library(tidymodels)
 library(vip)
+library(pdp)
+library(patchwork)
 
 data <- read_rds("data/regression/df.rds")
 
@@ -39,7 +41,7 @@ map(list(data_train, data_test), extract_continent_dist) |>
 
 # Evaluation using 10-fold CV using a couple of different metrics
 
-data_train_cv_split <- vfold_cv(data_train, v = 10, repeats = 5)
+data_train_cv_split <- vfold_cv(data_train, v = 10, repeats = 2)
 
 eval_metrics <- metric_set(rmse, mae, rsq)
 
@@ -51,7 +53,7 @@ rf_model <- rand_forest() |>
 
 rf_recipe <- recipe(MAT.0.Both ~ . , data = data_train) |>
   update_role(id, new_role = "ID") |>
-  step_impute_median(all_numeric_predictors()) |> 
+  step_impute_median(all_numeric_predictors()) |>
   step_corr(all_numeric_predictors())
 
 rf_workflow <- workflow() |>
@@ -75,34 +77,100 @@ rf_fit |>
 
 # More extensive tuning with a gradient boosted model
 
-gbm_model <- boost_tree(trees = 200,
+gbm_model <- boost_tree(trees = 500,
                         learn_rate = 0.05,
                         sample_size = 0.75) |>
   set_mode("regression") |>
-  set_engine("xgboost", objective = "reg:squarederror")
+  set_engine(
+    "xgboost",
+    objective = "reg:squarederror",
+    alpha = tune(),
+    lambda = tune()
+  )
 
 gbm_recipe <- recipe(MAT.0.Both ~ . , data = data_train) |>
   update_role(id, new_role = "ID") |>
-  step_impute_median(all_numeric_predictors()) |> 
-  step_corr(all_numeric_predictors()) |> 
+  step_impute_median(all_numeric_predictors()) |>
+  step_corr(all_numeric_predictors()) |>
   step_dummy(all_factor_predictors(), one_hot = T)
 
 gbm_workflow <- workflow() |>
   add_recipe(gbm_recipe) |>
   add_model(gbm_model)
 
-# Evaluate gbm model using 10-Fold CV and test set
+gbm_param_grid <- grid_regular(list(alpha = penalty_L1(), lambda = penalty_L2()), levels = 3)
 
-gbm_cv_fit <- gbm_workflow |>
-  fit_resamples(data_train_cv_split, metrics = eval_metrics)
+# Tune gbm model using 10-fold CV and a grid search and evaluate on test set
 
-collect_metrics(gbm_cv_fit)
+gbm_tuning_results <- gbm_workflow |>
+  tune_grid(data_train_cv_split, grid = gbm_param_grid)
 
-gbm_fit <- gbm_workflow |>
-  fit(data_train)
+collect_metrics(gbm_tuning_results) |>
+  mutate(alpha = factor(signif(alpha, 2))) |>
+  ggplot(aes(x = lambda, y = mean, color = alpha)) +
+  scale_x_log10() +
+  geom_line() +
+  facet_wrap(vars(.metric), scales = "free")
 
-vip(vi_model(gbm_fit, type = "gain"))
+show_best(gbm_tuning_results, metric = "rmse", n = 1)
+show_best(gbm_tuning_results, metric = "rsq", n = 1)
 
-gbm_fit |>
-  augment(data_test) |>
-  eval_metrics(truth = MAT.0.Both, estimate = .pred)
+# Fit the optimal model to training data and evaluate on test
+gbm_final_workflow <- gbm_workflow |>
+  finalize_workflow(select_best(gbm_tuning_results, metric = "rmse"))
+
+gbm_final_fit <- gbm_final_workflow |>
+  last_fit(data_init_split)
+
+collect_metrics(gbm_final_fit)
+
+# Assess feature importance and dependencies
+gbm_fitted_xgboost <- extract_fit_engine(gbm_final_fit)
+
+gbm_feat_imps <- vi_model(gbm_fitted_xgboost, type = "gain")
+
+vip(gbm_feat_imps)
+
+### Partial Dependence Plot utilities
+plot_1d_pdps <- function(model, features, train_data, model_name) {
+  pdp_data <- lapply(features, function(feat)
+    c(name = feat, data = list(
+      pdp::partial(model, pred.var = feat, train = train_data)
+    )))
+  
+  pdp_data <- lapply(pdp_data, function(l) {
+    names(l$data)[1] <- "x"
+    l
+  })
+  
+  pdp_plot_fun <- function(var_data) {
+    ggplot(as.data.frame(var_data$data), aes(x = x, y = yhat)) +
+      geom_line() +
+      ggtitle(var_data$name)
+  }
+  
+  y_values <- sapply(pdp_data, function(l)
+    l$data$yhat)
+  
+  y_max <- max(sapply(y_values, function(y)
+    max(y)))
+  y_min <- min(sapply(y_values, function(y)
+    min(y)))
+  
+  pdp_plots <- lapply(pdp_data, pdp_plot_fun)
+  
+  Reduce('+', pdp_plots) + plot_annotation(title = paste0("1-D PDP for ", model_name)) &
+    ylim(y_min, y_max)
+  
+}
+
+fitted_recipe <- extract_recipe(gbm_final_fit)
+
+data_train_baked <- bake(fitted_recipe, data_train) |>
+  select(all_of(gbm_fitted_xgboost$feature_names))
+
+# Top 5 most important variables univariate PDP plots
+plot_1d_pdps(gbm_fitted_xgboost,
+             gbm_feat_imps$Variable[1:5],
+             data_train_baked,
+             "GB Trees")
